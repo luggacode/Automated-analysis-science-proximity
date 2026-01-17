bf195df78776137f247d84da947460e401101e98
Luca Bevilacqua <lucarbevilacqua@icloud.com>
2026-01-17 18:53:51 -0500

    port added

--- Flask_app.py
+++ Flask_app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from bokeh.embed import components
 import API_call
 import useful_methods
+import os
 from find_similar_authors import generate_similar_authors_map
 
 app = Flask(__name__)
@@ -22,4 +23,5 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
